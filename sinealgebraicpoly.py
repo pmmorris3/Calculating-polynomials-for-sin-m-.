68a564656552d659e7c867c0318666030eac0b10
Peyton Morris <50498005+pmmorris3@users.noreply.github.com>
2020-04-10 12:46:23 -0500

    Update sinealgebraicpoly.py

--- sinealgebraicpoly.py
+++ sinealgebraicpoly.py
@@ -25,105 +25,149 @@ def binomial(n, k):
     for p in eratosthenes_simple_numbers(n):
         ans *= p ** (calc_pow_in_factorial(n, p) - calc_pow_in_factorial(k, p) - calc_pow_in_factorial(n - k, p))
     return ans
+def polyval(x, coef):
+    sum = Decimal(0)
+    count = 0
+    while 1:
+        sum = sum + coef[0]     
+        coef = coef[1:]         
+        if not coef: break      
+        sum = sum * x           
 
+    return sum
 
 class Sine:
 	def __init__(self, power,arg,coeff):
 		self.power = power
 		self.arg = arg
 		self.coeff = coeff
+class Poly:
+	def __init__(self,m):
+		self.m = m
 
+	def generatePolynomial(self):
+		listOfSines = []
+		condensedSines = []
+		for n in range(0,(90)//2+1):
+			for j in range(0,n+1):
+				z = complex(0,1) ** (90 - 2*n)
+				summand = Sine(Decimal(90+2*j-2*n),(self.m*Pi/180), Decimal(int(z.real)*binomial(90,2*n)* binomial(n,j) * (-1)**j))
+				listOfSines.append(summand)
+		listOfSines.sort(key=operator.attrgetter('power'))
 
+		for n in range(0,90 + 1):
+			if n % 2 == 0:
+				newSine = Sine(Decimal(n),self.m*Pi/180,(0))
+				for x in listOfSines:
+					if x.power == n:
+						newSine.coeff += x.coeff
 
-print("We claim sin(m°) is an algebraic number, whenever m  is an integer. Choose an m, and we will exhibit a polynomial over the rationals which sin(m°) satisfies.")
-m_in=int(input("Enter an integer for m: "))
-if m_in >= 0:
-	m = m_in % 360
-if m_in < 0:
-	m = m_in % -360
-listOfSines = []
-condensedSines = []
-for n in range(0,(90)//2+1):
-		for j in range(0,n+1):
-			z = complex(0,1) ** (90 - 2*n)
-			summand = Sine(Decimal(90+2*j-2*n),(m*Pi/180), Decimal(int(z.real)*binomial(90,2*n)* binomial(n,j) * (-1)**j))
-			listOfSines.append(summand)
-listOfSines.sort(key=operator.attrgetter('power'))
+				condensedSines.append(newSine)
+		return condensedSines
+	def printPolynomial(self):
+		summands = self.generatePolynomial()
+		if self.m % 180== 0:
+			print("sin(%d°) = 0 is clearly algebraic." % self.m)
 
-for n in range(0,90 + 1):
-	if n % 2 == 0:
-		newSine = Sine(Decimal(n),m*Pi/180,(0))
-		for x in listOfSines:
-			if x.power == n:
-				newSine.coeff += x.coeff
+		if self.m %4 ==2 :
+			for x in summands:
+				if x.power == 0:
+					print("2", " ")
+				elif x.coeff == 1:
+					print("+ %dsin(%d°)^%d" % (x.coeff,self.m, x.power), " ")
 
-		condensedSines.append(newSine)
+				elif x.coeff < 0:
+					print("- %d*sin(%d°)^%d" %(-x.coeff,self.m, x.power),  " ")
+				else:
+					print("+ %d*sin(%d°)^%d" %(x.coeff,self.m, x.power),  " ")
+		if self.m %4 ==0 and self.m % 180 != 0:
+			for x in summands:
+				if x.power == 0:
+					continue
+				elif x.coeff == 1:
+					print("+ %dsin(%d°)^%d" % (x.coeff,self.m, x.power), " ")
 
-sum = Decimal(0.0)
+				elif x.coeff < 0:
+					print("- %d*sin(%d°)^%d" %(-x.coeff,self.m, x.power),  " ")
+				else:
+					print("+ %d*sin(%d°)^%d" %(x.coeff,self.m, x.power),  " ")
+		if self.m %4 ==3 or (self.m % 4 == 1 and self.m != 1):
+			for x in summands:
+				if x.power == 0:
+					print("1", " ")
+				elif x.coeff == 1:
+					print("+ %dsin(%d°)^%d" % (x.coeff,self.m, x.power), " ")
 
-if m== 0:
-	print("sin(%d°) = 0 is clearly algebraic." % m_in)
-	sys.exit(0)
+				elif x.coeff < 0:
+					print("- %d*sin(%d°)^%d" %(-x.coeff,self.m, x.power),  " ")
+				else:
+					print("+ %d*sin(%d°)^%d" %(x.coeff,self.m, x.power),  " ")
 
-if m %4 ==2 :
-	for x in condensedSines:
-		if x.power == 0:
-			print("2", " ")
-		elif x.coeff == 1:
-			print("+ %dsin(%d°)^%d" % (x.coeff,m, x.power), " ")
+		if self.m == 1:
+			for x in summands:
+				if x.power == 0:
+					print("1", " ")
+				elif x.coeff == 1:
+					print("+ %dsin(1°)^%d" % (x.coeff, x.power), " ")
 
-		elif x.coeff < 0:
-			print("- %d*sin(%d°)^%d" %(-x.coeff,m, x.power),  " ")
+				elif x.coeff < 0:
+					print("- %d*sin(1°)^%d" %(-x.coeff, x.power),  " ")
+				else:
+					print("+ %d*sin(1°)^%d" %(x.coeff, x.power),  " ")
+
+	def evaluatePolynomial(self):
+		summands = self.generatePolynomial()
+		summands.sort(key=operator.attrgetter('power'))
+		coeff = []
+		n = 90
+		while n >= 0:
+			if n % 2 == 0:
+				coeff.append(Decimal(summands[n//2].coeff))
+			else:
+				coeff.append(0)
+			n -= 1
+		if self.m % 4 == 2:
+			return polyval(Decimal(math.sin(self.m * Pi/180)), coeff) + 1
+		if self.m % 4 == 0:
+			return polyval(Decimal(math.sin(self.m * Pi/180)), coeff) - 1
 		else:
-			print("+ %d*sin(%d°)^%d" %(x.coeff,m, x.power),  " ")
-		sum += x.coeff*(Decimal(math.sin(x.arg)) ** Decimal(x.power))
-if m %4 ==0:
-	for x in condensedSines:
-		if x.power == 0:
-			continue
-		elif x.coeff == 1:
-			print("+ %dsin(%d°)^%d" % (x.coeff,m, x.power), " ")
+			return polyval(Decimal(math.sin(self.m * Pi/180)), coeff)
 
-		elif x.coeff < 0:
-			print("- %d*sin(%d°)^%d" %(-x.coeff,m, x.power),  " ")
+
+
+
+
+stop = " "
+while (stop.lower() != "no" ):
+	stop = " "
+	print("We claim sin(m°) is an algebraic number, whenever m  is an integer. Choose an m, and we will exhibit a polynomial over the rationals which sin(m°) satisfies.")
+	degrees_in=input("Enter an integer for m: ")
+	while (degrees_in.isnumeric() == 0):
+		if (degrees_in[0] in {'+','-'} and degrees_in[1::].isnumeric() == 1):
+			break
 		else:
-			print("+ %d*sin(%d°)^%d" %(x.coeff,m, x.power),  " ")
-		sum += x.coeff*(Decimal(math.sin(x.arg)) ** Decimal(x.power))
-if m %4 ==3 or (m % 4 == 1 and m != 1):
-	for x in condensedSines:
-		if x.power == 0:
-			print("1", " ")
-		elif x.coeff == 1:
-			print("+ %dsin(%d°)^%d" % (x.coeff,m, x.power), " ")
+			degrees_in=input("Enter an integer for m: ")
+	print("\n"*100)
 
-		elif x.coeff < 0:
-			print("- %d*sin(%d°)^%d" %(-x.coeff,m, x.power),  " ")
-		else:
-			print("+ %d*sin(%d°)^%d" %(x.coeff,m, x.power),  " ")
-		sum += x.coeff*(Decimal(math.sin(x.arg)) ** Decimal(x.power))
+	degrees_in = int(degrees_in)
+	if degrees_in >= 0:
+		degrees = degrees_in % 360
+	if degrees_in < 0:
+		degrees = degrees_in % -360
+	p_m = Poly(degrees)
+	p_m.printPolynomial()
+	sum = p_m.evaluatePolynomial()
+	if degrees %180 != 0:
+		print("\n","Your computer approximates this as: \n",sum)
 
-if m == 1:
-	for x in condensedSines:
-		if x.power == 0:
-			print("1", " ")
-		elif x.coeff == 1:
-			print("+ %dsin(1°)^%d" % (x.coeff, x.power), " ")
+	stop = input("Would you like to try another? Enter yes or no: ")
+	if stop != "no":
+		while (stop.lower() != "yes"):
+			stop = input("Would you like to try another? Enter yes or no: ")
+			if stop == "no":
+				break
+	print("\n"*100)
 
-		elif x.coeff < 0:
-			print("- %d*sin(1°)^%d" %(-x.coeff, x.power),  " ")
-		else:
-			print("+ %d*sin(1°)^%d" %(x.coeff, x.power),  " ")
-		sum += x.coeff*(Decimal(math.sin(x.arg)) ** Decimal(x.power))
-
-
-
-if m % 4 == 2:
-	sum += Decimal(1)
-
-
-print("= 0")
-print("\n"*10,"Using a precision of 60 digits, your computer approximates this as: \n",sum)
-print("\n")
 print("It turns out one needs only three distinct polynomials, depending on the remainder upon division of m by 4.")
 print("Curiously, in the case m % 4 == 1,3, the corresponding polynomial only has roots of the form sin(m°), each of multiplicity 1.")
 print("In other cases, there are an additional 24 (possibly degenerate) roots which need not be of this form.")
